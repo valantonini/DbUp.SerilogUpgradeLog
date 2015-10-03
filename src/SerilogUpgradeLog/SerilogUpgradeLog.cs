f595586fd096f658bd4ed1ca28bdb5b5f0a9e4de
val antonini <val.antonini@gmail.com>
2015-10-03 17:51:49 +1000

    DbUp Serilog Upgrade Log

---
+++ src/SerilogUpgradeLog/SerilogUpgradeLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DbUp.Engine.Output;
+using Serilog;
+
+namespace SerilogUpgradeLog
+{
+    public class SerilogUpgradeLog : IUpgradeLog
+    {
+        public void WriteInformation(String format, params Object[] args)
+        {
+            Log.Information(String.Format(format, args));
+        }
+
+        public void WriteError(String format, params Object[] args)
+        {
+            Log.Error(format, args);
+        }
+
+        public void WriteWarning(String format, params Object[] args)
+        {
+            Log.Warning(format, args);
+        }
+    }
+}
